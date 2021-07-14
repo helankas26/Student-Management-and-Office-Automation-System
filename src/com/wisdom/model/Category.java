1a691245f14295acdfea204092ce1c0f5eebe336
HelankaS26 <helankas26@gmail.com>
2021-07-14 11:39:10 +0530

    Encapsulate Category class

--- src/com/wisdom/model/Category.java
+++ src/com/wisdom/model/Category.java
@@ -11,6 +11,25 @@ package com.wisdom.model;
  */
 public class Category {
     
-    String categoryID;
-    String categoryName;
+    private String categoryID;
+    private String categoryName;
+
+    public Category() {
+    }
+
+    public String getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(String categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
 }
